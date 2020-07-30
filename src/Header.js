5949ddd17271585ebd98e7f4c50bb1a8f5968cd1
Natalia Mateo <nmateo7@gmail.com>
2020-07-30 13:10:57 +0200

    Add html and css header

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,45 @@ import './Header.scss';
 const Header = () => {
   return (
     <header className='header'>
-      <nav></nav>
+      <nav className='header__nav'>
+        <ul className='header__nav--list'>
+          <li className='list__links'>
+            <a href='' alt='Inicio'>
+              <div className='list__container'>
+                <i class='fas fa-home list__container--icon'></i>Inicio
+              </div>
+            </a>
+          </li>
+          <li className='list__links'>
+            <a href='' alt='Perfil'>
+              <div className='list__container'>
+                <i class='far fa-user list__container--icon profile'></i>Perfil
+              </div>
+            </a>
+          </li>
+          <li className='list__links'>
+            <a href='' alt='Perfil'>
+              <div className='list__container'>
+                <i class='far fa-envelope list__container--icon'></i>Mensajes
+              </div>
+            </a>
+          </li>
+          <li className='list__links'>
+            <a href='' alt='Perfil'>
+              <div className='list__container'>
+                <i class='fas fa-user-friends list__container--icon friends'></i>Amigos
+              </div>
+            </a>
+          </li>
+          <li className='list__links'>
+            <a href='' alt='Perfil'>
+              <div className='list__container'>
+                <i class='fas fa-cog list__container--icon'></i>Ajustes
+              </div>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
