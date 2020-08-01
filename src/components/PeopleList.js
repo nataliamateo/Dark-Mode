e3c29fd7b02a91afadf7094c12147ae8ae622ed1
Natalia Mateo <nmateo7@gmail.com>
2020-08-01 15:50:03 +0200

    Add people props

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -5,11 +5,12 @@ import People from './People';
 const PeopleList = () => {
   return (
     <div className='people-container'>
-      <People />
-      <People />
-      <People />
-      <People />
-      <People />
+      <People name='1' />
+      <People name='2' />
+      <People name='3' />
+      <People name='4' />
+      <People name='5' />
+      <People name='6' />
     </div>
   );
 };
