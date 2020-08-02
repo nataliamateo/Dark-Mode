eaf37e32efbe1607a2218c160c96ada3faf50e5b
Natalia Mateo <nmateo7@gmail.com>
2020-08-02 12:50:50 +0200

    Add div on header html

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,35 +9,40 @@ const Header = () => {
           <li className='list__links'>
             <a href='#Inicio' alt='Inicio'>
               <div className='list__container'>
-                <i className='fas fa-home list__container--icon'></i>Inicio
+                <i className='fas fa-home list__container--icon'></i>
+                <div>Inicio </div>
               </div>
             </a>
           </li>
           <li className='list__links'>
             <a href='#Perfil' alt='Perfil'>
               <div className='list__container'>
-                <i className='far fa-user list__container--icon profile'></i>Perfil
+                <i className='far fa-user list__container--icon profile'></i>
+                <div>Perfil </div>
               </div>
             </a>
           </li>
           <li className='list__links'>
             <a href='#Mensajes' alt='Mensajes'>
               <div className='list__container'>
-                <i className='far fa-envelope list__container--icon'></i>Mensajes
+                <i className='far fa-envelope list__container--icon'></i>
+                <div>Mensajes</div>
               </div>
             </a>
           </li>
           <li className='list__links'>
             <a href='#Amigos' alt='Amigos'>
               <div className='list__container'>
-                <i className='fas fa-user-friends list__container--icon friends'></i>Amigos
+                <i className='fas fa-user-friends list__container--icon friends'></i>
+                <div> Amigos</div>
               </div>
             </a>
           </li>
           <li className='list__links'>
             <a href='#Ajustes' alt='Ajustes'>
               <div className='list__container'>
-                <i className='fas fa-cog list__container--icon'></i>Ajustes
+                <i className='fas fa-cog list__container--icon'></i>
+                <div>Ajustes</div>
               </div>
             </a>
           </li>
